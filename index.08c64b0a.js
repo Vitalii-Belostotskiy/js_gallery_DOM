8122ae52a5fc142828f6ea5457f00f83751240d9
Vitalii Bilostotskyi <belostotskiyvitalii84@gmail.com>
2024-09-05 15:13:37 +0300

    make build

---
+++ index.08c64b0a.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.onclick=function(e){e.preventDefault();var l=e.target.closest("a");l&&(t.src=l.href,t.alt=l.title)};
+//# sourceMappingURL=index.08c64b0a.js.map
